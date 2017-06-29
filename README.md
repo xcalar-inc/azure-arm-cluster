14e8d3cc52cd137c18c01e3085698164d6faf019
Amit Bakshi <abakshi@xcalar.com>
2017-06-28 19:48:31 -0700

    Add bootstrap script

--- README.md
+++ README.md
@@ -9,8 +9,9 @@ Once the trial has concluded, the Xcalar Compute Engine features will be disable
     target="_blank">
     <img src="http://azuredeploy.net/deploybutton.png" />
 </a>
-<a href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fxcalar-inc%2Fazure-arm-cluster%2Fmaster%2Fazuredeploy.json"
+
+<a href="http://armviz.io/#/?load=https%3A%2F%2Fraw.githubusercontent.com%2Fxcalar-inc%2Fazure-arm-cluster%2Fmaster%2Fazuredeploy.json"
     target="_blank">
-    <img src="http://azuredeploy.net/AzureGov.png" />
+    <img src="http://armviz.io/visualizebutton.png"/>
 </a>
 
---
+++ xcalar.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+INSTALLER_URL="$1"
+CLUSTER="$2"
+INDEX="$3"
+COUNT="$4"
+
+XLRROOT=/mnt/xcalar
+DOMAIN="$(dnsdomainname)"
+
+setenforce Permissive
+sed -i -e 's/^SELINUX=enforcing.*$/SELINUX=permissive/g' /etc/selinux/config
+
+yum update -y
+yum install -y nfs-utils epel-release parted
+yum install -y jq
+
+
+
+if mountpoint -q /mnt/resource; then
+    PART="$(findmnt -n /mnt/resource  | awk '{print $2}')"
+    umount /mnt/resource
+    DEV="${PART%[1-9]}"
+    parted $DEV -s 'rm 1 mklabel gpt mkpart primary 1 -1'
+    mkfs.ext4 -L data -E lazy_itable_init=0,lazy_journal_init=0,discard $PART
+    UUID="$(blkid $PART | awk '{print $2}')"
+    echo "LABEL=data     /mnt/data   ext4    nobarrier,relatime  0   0" | tee -a /etc/fstab
+    mkdir -p /mnt/data
+    mount /mnt/data
+fi
+
+curl -H Metadata:True "http://169.254.169.254/metadata/instance?api-version=2017-04-02&format=json" | jq . > metadata.json
+NETWORK="$(<metadata.json jq -r '.network.interface[].ipv4.subnet[].address')"
+MASK="$(<metadata.json jq -r '.network.interface[].ipv4.subnet[].prefix')"
+
+if [ "$INDEX" = 0 ]; then
+    systemctl enable rpcbind
+    systemctl enable nfs-server
+    systemctl enable nfs-lock
+    systemctl enable nfs-idmap
+    systemctl start rpcbind
+    systemctl start nfs-server
+    systemctl start nfs-lock
+    systemctl start nfs-idmap
+
+    mkdir -p /mnt/data/xcalar
+    chmod 0777 /mnt/data/xcalar
+    echo "/mnt/data/xcalar      ${NETWORK}/${MASK}(rw,sync,no_root_squash,no_all_squash)" | tee /etc/exports
+    systemctl restart nfs-server
+    if firewall-cmd --state; then
+        firewall-cmd --permanent --zone=public --add-service=nfs
+        firewall-cmd --reload
+    fi
+fi
+
+# Set up the mount here, but don't mount until later
+mkdir -p $XLRROOT
+echo "${CLUSTER}0:/mnt/data/xcalar   $XLRROOT    nfs     defaults    0   0" | tee -a /etc/fstab
+
+mkdir -p /etc/default/xcalar
+
+
+
+curl -sSL "${INSTALLER_URL}" > xcalar-installer.sh
+
+if ! bash -x ./xcalar-installer.sh --nostart; then
+    echo >&2 "ERROR: Failed to install xcalar"
+    exit 1
+fi
+
+MEMBERS=()
+for NODEID in $(seq 0 $((COUNT-1))); do
+    MEMBERS+=("${CLUSTER}${NODEID}.${DOMAIN}")
+done
+
+/opt/xcalar/scripts/genConfig.sh /etc/xcalar/template.cfg - "${MEMBERS[@]}" | tee /etc/xcalar/default.cfg
+
+sed -r -i -e 's@^Constants.XcalarRootCompletePath=.*$@Constants.XcalarRootCompletePath='${XLRROOT}'@g' /etc/xcalar/default.cfg
+
+mount ${XLRROOT}
+
+service xcalar start
