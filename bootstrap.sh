b198123b4dfd1e2f82daa147babb9d34e7c497cb
Amit Bakshi <abakshi@xcalar.com>
2017-08-16 15:03:12 -0700

    Preparing for Azure Marketplace

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-INSTALLER_URL="$1"
-CLUSTER="${HOSTNAME%%[0-9]*}"
-INDEX="${3:-0}"
-COUNT="${4:-1}"
+COUNT="${1:-1}"
+INSTALLER_URL="$2"
+CLUSTER="${3:-${HOSTNAME%%[0-9]*}}"
+INDEX="${4:-0}"
 LICENSE="$5"
-NFSMOUNT="${6:-${CLUSTER}0:/srv/share}"
+NFSMOUNT="$6"
 
 # If on a single node instance, use the local host
 # as the server
-if [ -z "$NFSHOST" ] && [ "$COUNT" = 1 ]; then
+if [ -z "$NFSMOUNT" ] && [ "$COUNT" = 1 ]; then
     NFSMOUNT="${HOSTNAME}:/srv/share"
 else
     NFSMOUNT="${CLUSTER}0:/srv/share"
@@ -26,15 +26,14 @@ XCE_HOME="${XCE_HOME:-/mnt/xcalar}"
 XCE_CONFIG="${XCE_CONFIG:-/etc/xcalar/default.cfg}"
 XCE_LICENSEDIR="${XCE_LICENSEDIR:-/etc/xcalar}"
 
-setenforce Permissive
+if [ "$(getenforce)" = Enforcing ]; then
+    setenforce Permissive
+fi
 sed -i -e 's/^SELINUX=enforcing.*$/SELINUX=permissive/g' /etc/selinux/config
 
 yum update -y
 yum install -y nfs-utils epel-release parted curl
 yum install -y jq
-
-# Download the installer as soon as we can
-test -n "$INSTALLER_URL" && curl -sSL "$INSTALLER_URL" > installer.sh
 
 # Determine our CIDR by querying the metadata service
 curl -H Metadata:True "http://169.254.169.254/metadata/instance?api-version=2017-04-02&format=json" | jq . > metadata.json
@@ -59,6 +58,17 @@ if [ "$HOSTNAME" = "$NFSHOST" ]; then
         mount "$SHARE"
     fi
 
+    mkdir -p "${SHARE}/xcalar"
+    chmod 0777 "${SHARE}/xcalar"
+
+    # Download the installer as soon as we can and share it. We've seen perf issues (up to 20min)
+    # when we had multiple nodes trying to download the installer at the same time.
+    test -n "$INSTALLER_URL" && curl -sSL "$INSTALLER_URL" > "${SHARE}/xcalar/installer.sh"
+
+    # Export the share to everyone in our CIDR block and mark it
+    # as world r/w
+    echo "${SHARE}/xcalar      ${NETWORK}/${MASK}(rw,sync,no_root_squash,no_all_squash)" | tee /etc/exports
+
     # Ensure NFS is running
     systemctl enable rpcbind
     systemctl enable nfs-server
@@ -68,23 +78,9 @@ if [ "$HOSTNAME" = "$NFSHOST" ]; then
     systemctl start nfs-server
     systemctl start nfs-lock
     systemctl start nfs-idmap
-
-    # Export the share to everyone in our CIDR block and mark it
-    # as world r/w
-    mkdir -p "${SHARE}/xcalar"
-    chmod 0777 "${SHARE}/xcalar"
-    echo "${SHARE}/xcalar      ${NETWORK}/${MASK}(rw,sync,no_root_squash,no_all_squash)" | tee /etc/exports
-    systemctl restart nfs-server
     if firewall-cmd --state; then
         firewall-cmd --permanent --zone=public --add-service=nfs
         firewall-cmd --reload
-    fi
-fi
-
-if [ -n "$INSTALLER_URL" ] && [ -f "installer.sh" ]; then
-    if ! bash -x installer.sh --nostart; then
-        echo >&2 "ERROR: Failed to run installer"
-        exit 1
     fi
 fi
 
@@ -94,29 +90,46 @@ for NODEID in $(seq 0 $((COUNT-1))); do
     MEMBERS+=("${CLUSTER}${NODEID}")
 done
 
-/opt/xcalar/scripts/genConfig.sh /etc/xcalar/template.cfg - "${MEMBERS[@]}" | tee "$XCE_CONFIG"
-if ! test -e "${XCE_LICENSEDIR}/XcalarLic.key"; then
-    echo "$LICENSE" > "${XCE_LICENSEDIR}/XcalarLic.key"
-fi
+while :; do
+    ONLINE=()
+    for NODE in "${MEMBERS[@]}"; do
+        if host "${NODE}"; then
+            ONLINE+=($NODE)
+        fi
+    done
+    COUNT_ONLINE="${#ONLINE[@]}"
+    if [ $COUNT_ONLINE -eq $COUNT ]; then
+        break
+    fi
+    echo >&2 "Only ${COUNT_ONLINE}/${COUNT} members online ... sleeping"
+    sleep 10
+done
 
 # Set up the mount for XcalarRoot
 mkdir -p "$XCE_HOME"
 echo "${NFSMOUNT}/xcalar   $XCE_HOME    nfs     defaults    0   0" | tee -a /etc/fstab
-
-sed -r -i -e 's@^Constants.XcalarRootCompletePath=.*$@Constants.XcalarRootCompletePath='$XCE_HOME'@g' "$XCE_CONFIG"
-
-mount "$XCE_HOME"
-mkdir -p "${XCE_HOME}/members"
-echo "$LOCALIPV4        $(hostname -f)  $(hostname -s)" > "${XCE_HOME}/members/${INDEX}"
-while :; do
-    COUNT_ONLINE=$(find "${XCE_HOME}/members/" -type f | wc -l)
-    echo >&2 "Have ${COUNT_ONLINE}/${COUNT} nodes online"
-    if [ $COUNT_ONLINE -eq $COUNT ]; then
-        break
-    fi
-    echo >&2 "Sleeping ..."
-    sleep 5
+until mount "$XCE_HOME"; do
+    echo >&2 "Unable to mount ${NFSMOUNT}/xcalar ... sleeping"
+    sleep 10
 done
 
+mkdir -p "${XCE_HOME}/members"
+echo "$LOCALIPV4        $(hostname -f)  $(hostname -s)" | tee "${XCE_HOME}/members/${INDEX}"
+
+if [ -n "$INSTALLER_URL" ] && [ -f "${XCE_HOME}/installer.sh" ]; then
+    cp -v "${XCE_HOME}/installer.sh" .
+    if ! bash -x installer.sh --nostart; then
+        echo >&2 "ERROR: Failed to run installer"
+        exit 1
+    fi
+fi
+
+/opt/xcalar/scripts/genConfig.sh /etc/xcalar/template.cfg - "${MEMBERS[@]}" | tee "$XCE_CONFIG"
+sed -r -i -e 's@^Constants.XcalarRootCompletePath=.*$@Constants.XcalarRootCompletePath='$XCE_HOME'@g' "$XCE_CONFIG"
+if [ "${#LICENSE}" -gt 1 ]; then
+    if ! test -e "${XCE_LICENSEDIR}/XcalarLic.key"; then
+        echo "$LICENSE" > "${XCE_LICENSEDIR}/XcalarLic.key"
+    fi
+fi
 
 service xcalar start
